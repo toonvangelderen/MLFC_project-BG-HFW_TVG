eb25e5f95fffb9d16b65b57dd5b6275e62828845
toonvangelderen <80319208+toonvangelderen@users.noreply.github.com>
2023-05-11 11:09:59 +0200

    initial commit

---
+++ original_CS230_code/project/mcmc/metropolis.py
@@ -0,0 +1,166 @@
+import numpy as np
+
+#################################################################################
+
+class MetropolisSampler(object):
+
+    def __init__(self, model, x0, temperature = 1.0, burnin = 0, stride = 1, nwalkers = 1, 
+        mapper = None, **kwargs):
+        """Metropolis Monte-Carlo simulation.
+
+        Parameters
+        ----------
+        model : Energy model
+            Energy model object, must provide the function energy(x) and energy_idx(x)
+        x0 : [array]
+            Initial configuration
+        temperatures : float or array
+            Temperature. By default (1.0) the energy is interpreted in reduced units.
+            When given an array, its length must correspond to nwalkers, then the walkers
+            are simulated at different temperatures.
+        burnin : int
+            Number of burn-in steps that will not be saved
+        stride : int
+            Every so many steps will be saved
+        nwalkers : int
+            Number of parallel walkers
+        mapper : Mapper object
+            Object with function map(X), e.g. to remove permutation.
+            If given will be applied to each accepted configuration.
+
+        """
+        self.model = model
+        self.temperature = temperature
+        self.burnin = burnin
+        self.stride = stride
+        self.nwalkers = nwalkers
+        self.kwargs = kwargs
+
+        if mapper is None:
+            class DummyMapper(object):
+                def map(self, X):
+                    return X
+            mapper = DummyMapper()
+        self.mapper = mapper
+
+        # Set initial coordinates
+        self.reset(x0)
+
+    def _proposal_step(self):
+        # Proposal step
+
+        props = [self.model.step(xi, **self.kwargs) for xi in self.x]
+        self.idx_prop = np.array([p[0] for p in props])
+        self.x_prop = np.array([p[1] for p in props])
+        
+        self.E_idx = np.array([self.model.energy_idx(xi, i) for (xi, i) in zip(self.x, self.idx_prop)])
+        self.E_idx_prop = np.array([self.model.energy_idx(xi, i) for (xi, i) in zip(self.x_prop, self.idx_prop)])
+
+    def _acceptance_step(self):
+        # Acceptance step
+        dE = self.E_idx_prop - self.E_idx
+        acc = -np.log(np.random.rand()) > dE / self.temperature
+
+        for i in range(len(acc)):
+            self.x[i] = self.x_prop[i] if acc[i] else self.x[i]
+        self.E = self.E + np.where(acc, dE, 0.0)
+
+    def reset(self, x0):
+        # Counters
+        self.step = 0
+        self.steps_ = []
+        self.traj_ = []
+        self.etraj_ = []
+
+        # Initial configuration
+        self.x = np.tile(x0, tuple([self.nwalkers]) + tuple([1]) * len(x0.shape) )
+        self.x = self.mapper.map(self.x)
+        self.E = np.array([self.model.energy(xi) for xi in self.x])
+
+        # Save first frame if no burnin
+        if self.burnin == 0:
+            self.steps_.append(0)
+            self.traj_.append(self.x.copy())
+            self.etraj_.append(self.E / self.model.num_sites(self.x[0]))
+
+    @property
+    def steps(self):
+        return np.array(self.steps_)
+
+    @property
+    def trajs(self):
+        """Returns a list of trajectories, one trajectory for each walker."""
+        T = np.array(self.traj_).astype(np.float32)
+        return [T[:, i, :] for i in range(T.shape[1])]
+
+    @property
+    def traj(self):
+        return self.trajs[0]
+
+    @property
+    def etrajs(self):
+        """Returns a list of energy trajectories, one trajectory for each walker."""
+        E = np.array(self.etraj_)
+        return [E[:, i] for i in range(E.shape[1])]
+
+    @property
+    def etraj(self):
+        return self.etrajs[0]
+
+    def run(self, nsteps = 1, verbose = 0):
+        for i in range(nsteps):
+            self._proposal_step()
+            self._acceptance_step()
+            self.step += 1
+            if verbose > 0 and i % verbose == 0:
+                print('Step', i, '/', nsteps)
+            if self.step > 0 and self.step > self.burnin and self.step % self.stride == 0:
+                self.steps_.append(self.step)
+                self.traj_.append(self.x.copy())
+                self.etraj_.append(self.E / self.model.num_sites(self.x[0])) # Per-site energy trajectory
+
+
+class ReplicaMetropolisSampler(object):
+
+    def __init__(self, model, x0, temperatures, burnin = 0, stride = 1, mapper = None, **kwargs):
+        if temperatures.size % 2 == 0:
+            raise ValueError('Please use an odd number of temperatures.')
+
+        self.toggle = 0
+        self.temperatures = temperatures
+        self.sampler = MetropolisSampler(model, x0, temperature = temperatures,
+            burnin = burnin, stride = stride, nwalkers = temperatures.size, mapper = mapper, **kwargs)
+
+    @property
+    def steps(self):
+        splits = np.split(self.sampler.steps, 
+            1 + np.argwhere(self.sampler.steps[1:] < self.sampler.steps[:-1]).flatten())
+
+        steps = splits[0]
+        for i in range(1, len(splits)): 
+            steps = np.append(steps, splits[i-1][-1] + splits[i])
+        return steps
+
+    @property
+    def trajs(self):
+        return self.sampler.trajs
+
+    @property
+    def etrajs(self):
+        return self.sampler.etrajs
+
+    def run(self, nepochs = 1, nsteps_per_epoch = 1, verbose = 0):
+        for i in range(nepochs):
+            self.sampler.run(nsteps = nsteps_per_epoch)
+            # Exchange
+            for k in range(self.toggle, self.temperatures.size-1, 2):
+                c = -(self.sampler.E[k+1] - self.sampler.E[k]) * (1.0/self.temperatures[k+1] - 1.0/self.temperatures[k])
+                acc = -np.log(np.random.rand()) > c
+                if acc:
+                    h = self.sampler.x[k].copy()
+                    self.sampler.x[k] = self.sampler.x[k+1].copy()
+                    self.sampler.x[k+1] = h
+                    h = self.sampler.E[k]
+                    self.sampler.E[k] = self.sampler.E[k+1]
+                    self.sampler.E[k+1] = h
+            self.toggle = 1 - self.toggle
---
+++ original_CS230_code/project/networks/net.py
@@ -0,0 +1,97 @@
+"""Defines the neural network, losss function and metrics"""
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+# defining RealNVP network (https://github.com/senya-ashukha/real-nvp-pytorch/blob/master/real-nvp-pytorch.ipynb)
+class RealNVP(nn.Module): # base class Module
+    def __init__(self, nets, nett, mask, prior, system, input_dimension):
+        super(RealNVP, self).__init__()
+
+        self.prior = prior
+        self.mask = nn.Parameter(mask, requires_grad=False)
+        self.t = torch.nn.ModuleList([nett() for _ in range(len(mask))]) # translation function (net)
+        self.s = torch.nn.ModuleList([nets() for _ in range(len(mask))]) # scaling function (net)
+        # nn.ModuleList is basically just like a Python list, used to store a desired number of nn.Module’s.
+        self.logp = 1.0 # initialize to 1
+        self.system = system # class of what molecular system are we considering. E.g. Ising.
+        self.orig_dimension = input_dimension # tuple describing original dim. of system. e.g. Ising Model with N = 8 would be (8,8)
+
+    def g(self, z):
+        log_R_zx, x = z.new_zeros(z.shape[0]), z
+
+        for i in range(len(self.t)): # for each layer
+            x_ = x*self.mask[i] # splitting features between channels.
+                                # features selected here used to compute s(x) and f(x) but not updated themselves yet.
+            s = self.s[i](x_)*(1 - self.mask[i])
+            t = self.t[i](x_)*(1 - self.mask[i])
+            x = x_ + (1 - self.mask[i]) * (x * torch.exp(s) + t)
+            log_R_zx += torch.sum(s,-1)
+        return x, log_R_zx
+
+    def f(self, x):
+        log_R_xz, z = x.new_zeros(x.shape[0]), x
+
+        # new_zeros(size) returns a Tensor of size "size" filled with 0s
+        for i in reversed(range(len(self.t))): # move backwards through layers
+            z_ = self.mask[i] * z # tensor of size num samples x num features
+            s = self.s[i](z_) * (1-self.mask[i]) # self.s[i] is the entire sequence of scaling operations
+            t = self.t[i](z_) * (1-self.mask[i])
+            z = (1 - self.mask[i]) * (z - t) * torch.exp(-s) + z_
+            log_R_xz -= s.sum(dim=-1)
+            # each pass through here applies all operations defined in nets() and all the ones defined in nett()
+        # self.s[1](z_) is not the same as self.s[3](z_)
+        self.log_R_xz = log_R_xz # save so we can reference it later
+        return z, log_R_xz
+
+    def forward(self, x):
+        z, self.logp = self.f(x)
+        return z
+
+    def log_prob(self,x):
+        z, logp = self.f(x) # z = f(x)
+        return self.prior.log_prob(z) + logp
+
+    def sample(self, batchSize):
+        z = self.prior.sample_n(batchSize)
+        logp = self.prior.log_prob(z)
+        x, log_R_zx = self.g(z)
+        return z.detach().numpy() , x.detach().numpy()
+
+    def loss(self, batch, w_ml = 1.0, w_kl = 0.0, w_rc = 0.0):
+        return w_ml*self.loss_ml(batch) + w_kl*self.loss_kl(batch) + w_rc*self.loss_rc(batch)
+
+    def loss_ml(self, batch):
+        z, log_R_xz = self.f(batch)
+        self.energies = self.calculate_energy(batch)
+        return self.expected_value(0.5*torch.norm(z,dim=1)**2 - log_R_xz)
+
+    def loss_kl(self, batch_z):
+        x, log_R_zx = self.g(batch_z)
+        self.energies = self.calculate_energy(x)
+        return self.expected_value(self.energies - log_R_zx)
+
+    def loss_kl_ising(self, batch_z):
+        x, log_R_zx = self.g(batch_z)
+        self.energies = self.calculate_energy(torch.sign(x))
+        return self.expected_value(self.energies - log_R_zx)
+
+    def calculate_energy(self, batch):
+        energies = batch.new_zeros(batch.shape[0])
+
+        e_high = 10**4
+        for i in range(batch.shape[0]): # for each x in the batch
+            config = batch[i,:].reshape(self.orig_dimension) # reshape into correct form
+            energies[i] = self.system.energy(config)
+            if abs(energies[i]) == float('inf'):
+                    print("energy overflow detected")
+            elif energies[i] > e_high:
+                    energies[i] = e_high + torch.log(energies[i] - e_high + 1.0)
+
+        self.weights = torch.exp(-energies*0.0) # simple average
+        return energies
+
+    def expected_value(self, observable):
+        return torch.dot(observable,self.weights)/torch.sum(self.weights)
